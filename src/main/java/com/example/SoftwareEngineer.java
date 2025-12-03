38941dba9f572468df148270c961c790b811f515
Naween Jasinghe <jasinghenaween@gmail.com>
2025-12-03 17:02:07 -0500

    more complex api

---
+++ src/main/java/com/example/SoftwareEngineer.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer(String techStack, String name, Integer id) {
+        this.techStack = techStack;
+        this.name = name;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
---
+++ src/main/java/com/example/SoftwareEngineerController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer("js, node, react, tailwind", "James", 1),
+                new SoftwareEngineer("Java, spring, tailwind", "Jamila", 2));
+    }
+}
